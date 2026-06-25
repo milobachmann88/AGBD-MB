8a9adea3183378d9cf5e702c3aadc84178ca5069
milo <loli.bachmann88@gmail.com>
2026-06-25 09:34:38 -0300

    tp terminado

--- proyecto/TP2.py
+++ proyecto/TP2.py
@@ -2,8 +2,7 @@
 
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as pit
-
+import matplotlib.pyplot as plt
 
 #importando csv
 df=pd.read_csv("fifa_23_280922.csv")
@@ -49,42 +48,49 @@ else:
 df_top_50=df.nlargest(50,'Acceleration')
 
 sns.set_theme(style="whitegrid")
-pit.figure(figsize=(9,5))
+plt.figure(figsize=(9,5))
 sns.barplot(
     data=df_top_50,
     x="Nom",
     y="Acceleration",
     estimator= sum,
     errorbar=None,
+    hue= "Nom",
     palette="magma",
 )
-pit.title(
+plt.title(
     "Distribucion de aceleración de cada club", fontsize=14
 )
-pit.xlabel("Nombre", fontsize=11)
-pit.ylabel("Aceleracion", fontsize=11)
+plt.xlabel("Nombre", fontsize=11)
+plt.ylabel("Aceleracion", fontsize=11)
 
-pit.tight_layout()
-pit.xticks(rotation=50, fontsize=3)
-pit.savefig("grafico_aceleración2.png", dpi=300, bbox_inches="tight")
-pit.close()
+plt.tight_layout()
+plt.xticks(rotation=50, fontsize=3)
+plt.savefig("grafico_aceleración.png", dpi=300, bbox_inches="tight")
+plt.close()
 print("Gráfico guardado como grafico_aceleración.png")
 
-#8
-datos_torta= df_filtrado.groupby("Pays")["Force"].sum()
-
-pit.figure(figsize=(7,7))
-pit.pie(
-    datos_torta,
-    labels=datos_torta.index,
+#8 
+df_top_5=df.nlargest(5,'Acceleration')
+plt.figure(figsize=(7,7))
+plt.pie(
+    df_top_5["Acceleration"],
+    labels=df_top_5["Nom"],
     autopct="%1.1f%%",
     colors=sns.color_palette("Set2"),
     startangle=140,
     wedgeprops={"edgecolor":"white", "linewidth":2}
 )
-pit.title("Distribucion interna de fuerza")
-pit.tight_layout()
-pit.xticks(rotation=50, fontsize=3)
-pit.savefig("grafico_fuerza3.png", dpi=300, bbox_inches="tight")
-pit.close()
-print("Gráfico guardado como grafico_fuerza3.png")
+plt.title("Jugadores con más aceleración")
+plt.tight_layout()
+plt.xticks(rotation=50, fontsize=3)
+plt.savefig("grafico_aceleracion2.png", dpi=300, bbox_inches="tight")
+plt.close()
+print("Gráfico guardado como grafico_aceleracion2.png")
+
+#9
+condicion_extra= df['Général']>70
+resultado=df.loc[filtro_avanzado & condicion_extra,
+                   ["Nom", "Général", "Pays"]]
+print(resultado)
+print(f"\nFilas seleccionadas: {len(resultado)}")
